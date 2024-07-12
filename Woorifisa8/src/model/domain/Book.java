1e9e6a4007b5312475511e47918fd641c33508d4
hyleei <qkrgustjtj@naver.com>
2024-07-12 12:14:26 +0900

    Book서비스기능구현ver1

--- Woorifisa8/src/model/domain/Book.java
+++ Woorifisa8/src/model/domain/Book.java
@@ -1,5 +1,22 @@
+
 package model.domain;
 
-public class Book {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Book {
+	
+	private String isbn;
+	private String title;
+	private String author;
+	private boolean status; // true : 대여중, false : 대출가능
+	
 }
+
--- Woorifisa8/src/service/LibraryService.java
+++ Woorifisa8/src/service/LibraryService.java
@@ -1,5 +1,23 @@
 package service;
 
-public class LibraryService {
+import lombok.RequiredArgsConstructor;
+import model.domain.Book;
+import model.domain.User;
+import repository.BookRepository;
 
+@RequiredArgsConstructor
+public class LibraryService {
+	private final BookRepository bookRepository;
+
+	public Book borrowBook (String Isbn, User user) {
+		if(user.getRole().equals("USER")==true) {
+			Book book = bookRepository.findBookByIsbn(Isbn)
+			if(book!=null) {
+				user.
+			}
+			else return null;
+		}
+		return null;
+		
+	}
 }
