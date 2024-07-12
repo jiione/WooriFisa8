9d2b2390557c269efb8a76f4e0731a2af26a4124
hyleei <qkrgustjtj@naver.com>
2024-07-12 11:42:51 +0900

    BookRepository 기능 구현

--- Woorifisa8/src/repository/BookRepository.java
+++ Woorifisa8/src/repository/BookRepository.java
@@ -1,5 +1,37 @@
 package repository;
 
-public class BookRepository {
+import java.util.ArrayList;
 
+import model.domain.Book;
+
+public class BookRepository {
+	private static ArrayList<Book> bookList = new ArrayList<>();
+	
+	public boolean deleteBookByIsbn(String Isbn) {
+		for(Book book : bookList) {
+			if(Isbn.equals(book.getIsbn()) == true) {
+				bookList.remove(book);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Book insertBook(Book book) {
+		bookList.add(book);
+		return book;
+	}
+	
+	public ArrayList<Book> findAll() {
+		return bookList;
+	}
+	
+	public Book findBookByIsbn(String Isbn) {
+		for(Book book : bookList) {
+			if(Isbn.equals(book.getIsbn()) == true) {
+				return book;
+			}
+		}
+		return null;
+	}
 }
