e9c272739a7cbf1fb3f5cc9065d4ec9df7c1d8bd
lisiant <wkdxod321@gmail.com>
2024-07-12 16:47:33 +0900

    fix git conflict

--- Woorifisa8/src/view/EndView.java
+++ Woorifisa8/src/view/EndView.java
@@ -1,13 +1,21 @@
 package view;
 
 import model.domain.Book;
+import model.domain.User;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class EndView {
 	public static void bookListView(ArrayList<Book> books) {
 		for (Book book : books) {
 			System.out.println(book.toString());
+		}
+	}
+	
+	public static void userListView(Map<String, User> users) {
+		for (User user : users.values()) {
+			System.out.println(user.toString());
 		}
 	}
 
