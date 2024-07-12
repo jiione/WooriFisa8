e9eb91d5fbd5236051ce2f73326d42db879a3a5a
Jeongju Park <wnwjdqkr@gmail.com>
2024-07-12 14:22:30 +0900

    user repository, service

--- Woorifisa8/src/service/UserService.java
+++ Woorifisa8/src/service/UserService.java
@@ -1,7 +1,10 @@
 package service;
 
+import java.util.ArrayList;
+
 import lombok.Getter;
 import lombok.Setter;
+import model.domain.Book;
 import model.domain.User;
 import repository.UserRepository;
 
@@ -15,5 +18,30 @@ public class UserService {
     }
     
     
+    //유저 추가
+    public User addUser(String adminId, String adminPw, User user) {
+    	if (validateAdmin(adminId, adminPw)) {
+    		return userRepository.addUser(user);
+    	}
+    	return null;
+    }
     
+    //유저 삭제
+    public boolean deleteUser(String adminId, String adminPw, User user) {
+    	if (validateAdmin(adminId, adminPw)) {
+    		return userRepository.deleteUser(user.getId());
+    	}
+    	return false;
+    }
+    
+    //유저가 도서 대출
+    public Book borrowBook(Book book, User user) {
+    	return userRepository.borrowBook(book, user);
+    }
+    
+    
+    //유저가 도서 반납
+    public Book returnBook(Book book, User user) {
+    	return userRepository.returnBook(book, user);
+    }
 }
