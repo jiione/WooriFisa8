a402fddfc01ffb9a4374abfd1eb9f821a6e357bf
Jeongju Park <wnwjdqkr@gmail.com>
2024-07-12 11:31:34 +0900

    feat userdomain

--- Woorifisa8/src/model/domain/User.java
+++ Woorifisa8/src/model/domain/User.java
@@ -1,5 +1,17 @@
 package model.domain;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String role;
+    private String name;
+    private String id;
+    private String pw;
 }
