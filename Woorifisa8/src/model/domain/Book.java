2a73c61c2e075fc4a8d8feb52eb6095e63c5e5b3
jiwon <83341978+jiione@users.noreply.github.com>
2024-07-12 12:37:27 +0900

    Revert "Feat-Library서비스구현"

--- Woorifisa8/src/model/domain/Book.java
+++ Woorifisa8/src/model/domain/Book.java
@@ -5,14 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
-@Setter
 public class Book {
 	
 	private String isbn;
--- Woorifisa8/src/service/LibraryService.java
+++ Woorifisa8/src/service/LibraryService.java
@@ -1,7 +1,5 @@
 package service;
 
-import java.util.ArrayList;
-
 import lombok.RequiredArgsConstructor;
 import model.domain.Book;
 import model.domain.User;
@@ -13,38 +11,13 @@ public class LibraryService {
 
 	public Book borrowBook (String Isbn, User user) {
 		if(user.getRole().equals("USER")==true) {
-			Book book = bookRepository.findBookByIsbn(Isbn);
+			Book book = bookRepository.findBookByIsbn(Isbn)
 			if(book!=null) {
-				book.setStatus(true);
+				user.
 			}
 			else return null;
 		}
 		return null;
-	}
-	
-	public Book returnBook (String Isbn, User user) {
-		if(user.getRole().equals("USER")==true) {
-			Book book = bookRepository.findBookByIsbn(Isbn);
-			book.setStatus(false);
-		}
-		return null;
-	}
-	
-	public ArrayList<Book> searchAllBook() {
-		return bookRepository.findAll();
-	}
-	
-	public Book insertBook (Book book, User user) {
-		if(user.getRole().equals("ADMIN")==true) {
-			return bookRepository.insertBook(book);
-		}
-		return null;
-	}
-	
-	public boolean deleteBook(String isbn, User user) {
-		if(user.getRole().equals("ADMIN")==true) {
-			return bookRepository.deleteBookByIsbn(isbn);
-		}
-		return false;
+		
 	}
 }
